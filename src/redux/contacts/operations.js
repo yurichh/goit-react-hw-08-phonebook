98385c0f5e6f35134a89f4fe8f9698b2322bfcb6
unknown <velidchukaleksey@gmail.com>
2024-01-04 16:05:03 +0200

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,7 +5,7 @@ export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get('/contats');
+      const { data } = await axios.get('/contacts');
       return data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
